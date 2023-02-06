9cd0fa559d8708a845f0569606907516b6097800
SHARMARKE NUR <Snur206@yahoo.com>
2023-02-06 10:22:41 -0800

    initial commit

---
+++ GITDate/src/main/java/com/GITDate/GITDate/GitDateApplication.java
@@ -0,0 +1,13 @@
+package com.GITDate.GITDate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDateApplication.class, args);
+	}
+
+}
---
+++ GITDate/src/test/java/com/GITDate/GITDate/GitDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GITDate.GITDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
